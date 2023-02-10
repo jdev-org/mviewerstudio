725125768089745b2aa8b6ab8976f59bfd697b2d
gaetanbrl <gaetan.bruel@jdev.fr>
2023-02-10 10:32:00 +0100

    add index.html and allow to pass branch

--- srv/python/install_backend_python.sh
+++ srv/python/install_backend_python.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+apt install libxslt1-dev libxml2-dev
+pip install virtualenv
+
 # FULL PATH - WILL CLONE REPO TO THIS LOCATION
 # EX : /home/<user>/git
 WORKING_PATH="$1"
+
+# To use a specific branch - will use master if no set
+BRANCH="$2"
 
 MVIEWERSTUDIO_DIR="mviewerstudio"
 
@@ -12,14 +18,16 @@ if [ "${WORKING_PATH}" ]; then
     MVIEWERSTUDIO_DIR="$1/mviewerstudio"
 fi
 
+
 STATIC_DIR="${MVIEWERSTUDIO_DIR}/srv/python/mviewerstudio_backend/static"
 
 
-apt install libxslt1-dev libxml2-dev
-pip install virtualenv
-
 if [ ! -d "${MVIEWERSTUDIO_DIR}" ]; then
     git clone https://github.com/jdev-org/mviewerstudio.git
+    if [ "${BRANCH}" ]; then
+        cd "${MVIEWERSTUDIO_DIR}"
+        git checkout "${BRANCH}"
+    fi
 fi
 
 mkdir -p "${MVIEWERSTUDIO_DIR}/srv/python/mviewerstudio_backend/static/apps"
@@ -27,6 +35,7 @@ cp -r "${MVIEWERSTUDIO_DIR}/css" "${STATIC_DIR}"
 cp -r "${MVIEWERSTUDIO_DIR}/img" "${STATIC_DIR}"
 cp -r "${MVIEWERSTUDIO_DIR}/js" "${STATIC_DIR}"
 cp -r "${MVIEWERSTUDIO_DIR}/lib" "${STATIC_DIR}"
+cp -r "${MVIEWERSTUDIO_DIR}/index.html" "${STATIC_DIR}"
 
 cp "${MVIEWERSTUDIO_DIR}/mviewerstudio.i18n.json" "${STATIC_DIR}/mviewerstudio.i18n.json"
 cp "${MVIEWERSTUDIO_DIR}/config-python-sample.json" "${STATIC_DIR}/apps/config.json"
@@ -37,3 +46,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt -r dev-requirements.txt
 pip install -e .
+
+echo "mviewerstudio | Install success !"
+exit 0
